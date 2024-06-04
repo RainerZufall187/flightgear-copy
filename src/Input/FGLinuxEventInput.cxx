RainerZufall187/flightgear-copy

commit 78f4930a65ba50b89f9692bf490d410940e72ec8

Fix build with 64-bit time_t on 32-bit architectures
In such configurations, input_event struct does not have time member,
and it has __sec and __usec members instead.

input_event_sec and input_event_usec are macros that expand to
time.tv_sec/time.tv_usec or __sec/__usec, depending on the time_t
configuration.

See this Linux commit for more information:
https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit?id=152194fe9c3f0323

src/Input/FGLinuxEventInput.cxx
@@ -420,8 +420,8 @@ void FGLinuxInputDevice::Send( const char * eventName, double value )
   evt.type=typeCode.type;
   evt.code = typeCode.code;
   evt.value = (long)value;
-  evt.time.tv_sec = 0;
-  evt.time.tv_usec = 0;
+  evt.input_event_sec = 0;
+  evt.input_event_usec = 0;
   size_t bytes_written = write(fd, &evt, sizeof(evt));
 
   if( bytes_written == sizeof(evt) )

commit 0e220de3bd615c334d13db65249c9eb4173fd590

FGLinuxEventInput: Fix missing input_event defines
Old kernel headers (prior to v4.16) don't have the input_event_sec and
input_event_usec macros defined in linux/input.h, resulting in build
failures on these systems (including the flightgear build server). Fix
these by checking whether the macros are defined, and if not defining
them explicitly.

Fixes: 78f4930a65ba ("Fix build with 64-bit time_t on 32-bit architectures")

src/Input/FGLinuxEventInput.cxx
@@ -39,6 +39,15 @@ extern "C" {
 
 #include <string.h>
 
+/*
+ * Handle old kernel headers (< v4.16) which don't define input_event_sec or
+ * input_event_usec macros (for use with struct input_event).
+ */
+#ifndef input_event_sec
+#define input_event_sec time.tv_sec
+#define input_event_usec time.tv_usec
+#endif
+
 struct TypeCode {
   unsigned type;
   unsigned code;