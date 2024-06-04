RainerZufall187/flightgear-copy

commit 68c2548608b3183d9cda50a50fafffc1ea2d39f3

Add passthrough shader for splash screen

src/Viewer/splash.cxx
@@ -79,6 +79,42 @@ SplashScreen::SplashScreen() :
     _splashLayer->setAlphaMode(osgXR::CompositionLayer::BLEND_ALPHA_UNPREMULT);
 #endif
 
+    const std::string vertex_source =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 pos;\n"
+        "layout(location = 3) in vec4 multitexcoord0;\n"
+        "\n"
+        "out vec2 texcoord;\n"
+        "\n"
+        "uniform mat4 osg_ModelViewProjectionMatrix;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = osg_ModelViewProjectionMatrix * pos;\n"
+        "    texcoord = multitexcoord0.st;\n"
+        "}\n";
+    osg::Shader* vertex_shader = new osg::Shader(osg::Shader::VERTEX, vertex_source);
+
+    const std::string fragment_source =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 fragColor;\n"
+        "\n"
+        "in vec2 texcoord;\n"
+        "\n"
+        "uniform sampler2D tex;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    fragColor = texture(tex, texcoord);\n"
+        "}\n";
+    osg::Shader* fragment_shader = new osg::Shader(osg::Shader::FRAGMENT, fragment_source);
+
+    _program = new osg::Program();
+    _program->addShader(vertex_shader);
+    _program->addShader(fragment_shader);
+
     setName("splashGroup");
     setUpdateCallback(new SplashScreenUpdateCallback);
 }
@@ -244,6 +280,16 @@ void SplashScreen::createNodes()
     stateSet->setRenderBinDetails(1000, "RenderBin");
     stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF);
 
+    bool use_vertex_attribute_aliasing = false;
+    if (guiCamera) {
+        auto gc = guiCamera->getGraphicsContext();
+        use_vertex_attribute_aliasing = gc->getState()->getUseVertexAttributeAliasing();
+    }
+    if (use_vertex_attribute_aliasing) {
+        stateSet->setAttribute(_program);
+        stateSet->addUniform(new osg::Uniform("tex", 0));
+    }
+
     if (useSRGB) {
         stateSet->setMode(GL_FRAMEBUFFER_SRGB, osg::StateAttribute::ON);
     }
@@ -359,6 +405,17 @@ osg::ref_ptr<osg::Camera> SplashScreen::createFBOCamera()
     stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF);
     stateSet->setAttribute(new osg::Depth(osg::Depth::ALWAYS, 0, 1, false));
     stateSet->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+
+    bool use_vertex_attribute_aliasing = false;
+    osg::Camera* guiCamera = flightgear::getGUICamera(flightgear::CameraGroup::getDefault());
+    if (guiCamera) {
+        auto gc = guiCamera->getGraphicsContext();
+        use_vertex_attribute_aliasing = gc->getState()->getUseVertexAttributeAliasing();
+    }
+    if (use_vertex_attribute_aliasing) {
+        stateSet->setAttribute(_program);
+        stateSet->addUniform(new osg::Uniform("tex", 0));
+    }
 
     return c;
 }

src/Viewer/splash.hxx
@@ -116,6 +116,8 @@ private:
     SGPropertyNode_ptr _splashAlphaNode;
     osg::ref_ptr<osg::Camera> _splashFBOCamera;
     osg::Vec3Array* _splashSpinnerVertexArray = nullptr;
+
+    osg::ref_ptr<osg::Program> _program;
     
     int _width, _height;
 

commit 521d35fa14162d290b7101e22c1258ce6b3d0ab4

Splash: Fix build on Mac
The SplashScreen::createNode() guiCamera local valiable isn't defined on
Mac, resulting in build failures.

Move it out of the ifdef.

Fixes: 68c2548608b3 ("Add passthrough shader for splash screen");

src/Viewer/splash.cxx
@@ -128,10 +128,10 @@ void SplashScreen::createNodes()
     // The splash FBO is rendered as sRGB, but for VR it needs to be in an sRGB
     // pixel format for it to be handled as such by OpenXR.
     bool useSRGB = false;
+    osg::Camera* guiCamera = flightgear::getGUICamera(flightgear::CameraGroup::getDefault());
 #if !defined(SG_MAC)
     // SRGB does detect on macOS, but doesn't actually work, so we
     // disable the check there.
-    osg::Camera* guiCamera = flightgear::getGUICamera(flightgear::CameraGroup::getDefault());
     if (guiCamera) {
         osg::GraphicsContext* gc = guiCamera->getGraphicsContext();
         osg::GLExtensions* glext = gc->getState()->get<osg::GLExtensions>();